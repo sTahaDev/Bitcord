02500e5c4d0fd20be8e43c61d312d3d2fdde9afc
Taha <105307821+sTahaDev@users.noreply.github.com>
2023-01-29 23:25:40 +0300

    bt1

---
+++ bitcord-main/users.js
@@ -0,0 +1,29 @@
+let user = [];
+
+user[0] = {
+    "id":"0",
+    "name": "Hasan",
+    "surname": "boz",
+    "yas": "19",
+    "pp": "./src/img/defPP.png"
+    
+
+}
+user[1] = {
+    "id":"1",
+    "name": "Taha",
+    "surname": "sahin",
+    "yas": "19",
+    "pp": "./src/img/kakashi.png"
+    
+
+}
+user[2] = {
+    "id":"2",
+    "name": "Taha",
+    "surname": "sahin",
+    "yas": "19",
+    "pp": "./src/img/mal.jpg"
+    
+
+}
